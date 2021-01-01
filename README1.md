2136cb220da642b21f482943066b2d14c9de33d2
Karime Pereida <karime.pereida@robotics.utias.utoronto.ca>
2020-12-31 19:00:54 -0500

    Modificaciones para poner los datos correctos

--- README1.md
+++
@@ -1,1 +0,0 @@
-# PKenia.gitbu.io
--- js/contact_me.js
+++ js/contact_me.js
@@ -63,8 +63,21 @@ $(function() {
     });
 });
 
-
 /*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {
     $('#success').html('');
 });
+
+// Initialize and add the map
+var mymap = L.map('mapid').setView([18.9352, -99.1939], 16);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia3BlcmVpZGEiLCJhIjoiY2tqYzQ3dTFlMHIxZjMxcXFjNmE1dzJlZCJ9.SfCqvPUzDiKaG2Bcp8MXaA', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+// To add pin
+var marker = L.marker([18.93293,-99.19276]).addTo(mymap);
+marker.bindPopup("<b>Dra. Kenia Pereida Pérez</b><br>Corporativo Cuernavaca, Torre 3 piso 9. Av. De los 50mts, colonia Villas Deportivas").openPopup();
